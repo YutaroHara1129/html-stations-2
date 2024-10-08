cc2a37238a948fc5e51cd3af0f909bc73befd5de
Yutaro Hara <77527605+aGO-MakeSomething@users.noreply.github.com>
2024-10-08 14:50:31 +0900

    [feat] Complete station13.

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const text = document.getElementById("text");
+  const checkbox = document.querySelector("#check");
+  if (checkbox.checked) {
+    text.style.backgroundColor = "red";
+  }
+  else {
+    text.style.backgroundColor = "white";
+  }
 }
